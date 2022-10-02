aa545eceb372f0b2b04fb0ff5182b09191895d35
alan <alan@alans-MacBook-Air.local>
2022-10-02 00:22:27 -0300

    Change bloom filter interface and use less hashing
    This PR change the cl-bloom-filter interface to receive two inputs (capacity and fp-rate).
    And based on those inputs calculates the number of bits and hash functions.

--- src/main.lisp
+++ src/main.lisp
@@ -2,33 +2,72 @@
   (:use :cl))
 (in-package :cl-bloom-filter)
 
+;;; Bloom Filter Helper Functions
+
+;; m  -> number of bits
+;; n  -> expected number of insertions
+;; fp -> false positive rate
+;;
+;; Based on https://en.wikipedia.org/wiki/Bloom_filter#Probability_of_false_positives
+;; optimal(m) = -(n*ln(e))/(ln(2))^2
+;;
+(defun %optimal-number-of-bits (n fp)
+  (ceiling (- (/ (* n (log fp)) (* (log 2) (log 2))))))
+
+;; n -> expected number of insertions 
+;; m -> number of bits
+;; k -> number of hash functions
+;;
+;; Based on https://en.wikipedia.org/wiki/Bloom_filter#Probability_of_false_positives
+;; optimal(k) = (m/n)*ln(2)
+;;
+(defun %optimal-number-of-hash-functions (n m)
+  (max 1 (round (* (/ m n) (log 2)))))
+
 ;;; Bloom Filter Class Definition
 
 (defclass bloom-filter ()
-  ((hash-functions
-    :initarg :hash-functions
-    :initform '(sxhash)
-    :accessor hash-functions)
-   (size
-    :initarg size
-    :initform 8000
-    :accessor size)
-   false-positive-rate
+  ((capacity
+    :initarg :capacity
+    :initform 10000
+    :accessor capacity)
+   (fp-rate
+    :initarg :fp-rate
+    :initform 1/1000
+    :accessor fp-rate)
+   (hash-function
+    :initarg :hash-function
+    :initform 'sxhash
+    :accessor hash-function)
    (hash-set
-    :accessor hash-set)))
+    :accessor hash-set)
+   (number-of-hash-functions
+    :accessor number-of-hash-functions)))
 
+;; n  -> expected number of insertions 
+;; m  -> number of bits
+;; k  -> number of hash functions
+;; fp -> false positive rate
 (defmethod initialize-instance :after ((bloom-filter bloom-filter) &key)
-  (setf (slot-value bloom-filter 'hash-set) (make-array (size bloom-filter) :element-type 'bit)))
+  (let* ((n (capacity bloom-filter))
+         (fp (fp-rate bloom-filter))
+         (m (%optimal-number-of-bits n fp))
+         (k (%optimal-number-of-hash-functions n m)))
+    (setf (slot-value bloom-filter 'number-of-hash-functions) k)
+    (setf (slot-value bloom-filter 'hash-set) (make-array m :element-type 'bit))))
 
 ;;; Bloom Filter Methods
 
 (defgeneric add (bloom-filter item) (:documentation "Add item to the bloom filter"))
 
-(defgeneric lookup (bloom-filter item) (:documentation "Check if item is on the bloom filter"))
+(defgeneric lookup (bloom-filter item) (:documentation "Check if item is member of the bloom filter"))
+
+(defmethod %size ((bloom-filter bloom-filter))
+  (length (hash-set bloom-filter)))
 
 (defmethod %hash ((bloom-filter bloom-filter) hash-function item)
   (let ((hashed-item (funcall hash-function item)))
-    (floor (mod hashed-item (size bloom-filter)))))
+    (floor (mod hashed-item (%size bloom-filter)))))
 
 (defmethod %set-bit ((bloom-filter bloom-filter) hashed-item)
   (setf (aref (hash-set bloom-filter) hashed-item) 1))
@@ -36,16 +75,30 @@
 (defmethod %get-bit ((bloom-filter bloom-filter) hashed-item)
   (aref (hash-set bloom-filter) hashed-item))
 
-(defmethod add ((bloom-filter bloom-filter) (item number))
-  (dolist (hash-function (hash-functions bloom-filter))
-    (let ((hashed-item (%hash bloom-filter hash-function item)))
-      (%set-bit bloom-filter hashed-item))))
+(defmethod add ((bloom-filter bloom-filter) item)
+  (let* ((number-of-hash-functions (number-of-hash-functions bloom-filter))
+         (size (%size bloom-filter))
+         (hash-function (hash-function bloom-filter))
+         (hash1 (funcall hash-function item))
+         (hash2 (funcall hash-function hash1)))
+    (do* ((i 1 (+ i 1)))
+         ((> i number-of-hash-functions) t)
+      ;; Based on Less Hashing paper: https://www.eecs.harvard.edu/~michaelm/postscripts/rsa2008.pdf
+      (let ((hashed-item (mod (abs (+ hash1 (* i hash2))) size)))      
+        (%set-bit bloom-filter hashed-item)))))
 
-(defmethod check ((bloom-filter bloom-filter) (item number))
-  (dolist (hash-function (hash-functions bloom-filter) t)
-    (let ((hashed-item (%hash bloom-filter hash-function item)))
-      (if (equal 0 (%get-bit bloom-filter hashed-item))
-          (return)))))
+(defmethod lookup ((bloom-filter bloom-filter) item)
+  (let* ((number-of-hash-functions (number-of-hash-functions bloom-filter))
+         (size (%size bloom-filter))
+         (hash-function (hash-function bloom-filter))
+         (hash1 (funcall hash-function item))
+         (hash2 (funcall hash-function hash1)))
+    (do* ((i 1 (+ i 1)))
+         ((> i number-of-hash-functions) t)
+      ;; Based on Less Hashing paper: https://www.eecs.harvard.edu/~michaelm/postscripts/rsa2008.pdf
+      (let ((hashed-item (mod (abs (+ hash1 (* i hash2))) size)))
+        (if (equal 0 (%get-bit bloom-filter hashed-item))
+            (return))))))
 
 #+nil
 (defvar abloom-filter (make-instance 'bloom-filter))
